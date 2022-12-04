03728ba50a4593163ad81bb854cbd63af8c88066
Maxim Samodurov <trueforce.go@gmail.com>
2022-12-04 16:55:54 +0400

    add JSON

--- AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -12,6 +12,32 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         
         collectionView.register(SearchResultsCell.self, forCellWithReuseIdentifier: cellId)
         
+        fetchItunesApps()
+        
+    }
+    
+    fileprivate func fetchItunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, resp, err in
+            
+            // if errors
+            if let err = err {
+                print("Error To Fetch The Data", err)
+                return
+            }
+            // if success
+            guard let data = data else { return }
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+            } catch {
+                print("Error with Decoding JSON", error)
+            }
+        }
+        .resume() // fires off the request
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -23,7 +49,8 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultsCell
+        cell.nameLabel.text = "HERE IS MY APP NAME"
         return cell
     }
     
