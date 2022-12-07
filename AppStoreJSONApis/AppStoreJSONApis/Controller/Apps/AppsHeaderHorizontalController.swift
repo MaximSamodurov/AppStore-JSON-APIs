9926f8b91e947da3db518dee27e411537f67af5e
Maxim Samodurov <trueforce.go@gmail.com>
2022-12-07 19:41:21 +0400

    Finished Apps Layout

--- AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/AppsHeaderHorizontalController.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/AppsHeaderHorizontalController.swift
@@ -1,25 +1,34 @@
 
 import UIKit
 
-class AppsHeaderHorizontalController: BaseListController {
+class AppsHeaderHorizontalController: BaseListController, UICollectionViewDelegateFlowLayout  {
     
     let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
         
-        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(AppsHeaderCell.self, forCellWithReuseIdentifier: cellId)
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: view.frame.height)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
-        return cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 16, bottom: 0, right: 0)
     }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
 }
--- AppStoreJSONApis/AppStoreJSONApis/Views/Apps/AppsPageHeader.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Views/Apps/AppsPageHeader.swift
@@ -1,20 +1,18 @@
 
 import UIKit
 
-
 class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        let appHeaderHorizontalController = AppsHeaderHorizontalController()
-                
         addSubview(appHeaderHorizontalController.view)
         appHeaderHorizontalController.view.fillSuperview()
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
